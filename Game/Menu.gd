e65440025c973030f477fb46b0245d62f786f9d4
Iddmeister <iddonagenda.t@gmail.com>
2022-03-21 22:00:22 +0000

    Slide Out Menu

---
+++ Game/Menu.gd
@@ -0,0 +1,16 @@
+extends PanelContainer
+
+
+func _ready():
+	pass
+
+
+func _on_MenuButton_pressed():
+	get_parent().get_node("MenuButton").hide()
+	$Animation.play("Show")
+
+
+func _on_Back_pressed():
+	$Animation.play("Hide")
+	yield($Animation, "animation_finished")
+	get_parent().get_node("MenuButton").show()
